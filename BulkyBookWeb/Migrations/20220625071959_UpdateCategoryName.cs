01f2968c05016d7b4ff77757c497df96e9e63b5a
haoyan <yuhaoyann@gmail.com>
2022-06-25 10:38:38 -0700

    set up category database

---
+++ BulkyBookWeb/Migrations/20220625071959_UpdateCategoryName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class UpdateCategoryName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Categories",
+                newName: "DisplayOrder");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayOrder",
+                table: "Categories",
+                newName: "MyProperty");
+        }
+    }
+}
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,7 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
