3562d4bad130086c2209afd9c57f9fbfb0633fd7
haoyan <yuhaoyann@gmail.com>
2022-06-27 13:51:15 -0700

    add partial view for notification

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -42,6 +42,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -77,6 +78,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -108,6 +110,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Remove(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
